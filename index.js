676ebafb458039401981e00bf7d84a78db63453d
Florencia Schumacher <florschumacher7@gmail.com>
2024-08-24 21:30:27 -0300

    ejercicio 1 ok

--- index.js
+++ index.js
@@ -1,106 +1,127 @@
 const urlApi = "https://thronesapi.com/api/v2/Characters";
-const fs = require('fs');
+
+const fs = require('fs').promises;
+
+
 
 //1) Realizar una función que permita recuperar la información del personaje “Ned Stark”
 
-async function obtenerNedStark(id) {
 
-    try{
-        const response = await fetch(`${urlApi}/${id}`);
+async function obtenerNedStark(nedStarkId){
+
+    try {
+        const response = await fetch(`${urlApi}/${nedStarkId}`);
+
         if (!response.ok){
-            throw new Error('Error! ' + response.status);
+            throw new Error('Ups, error! ' + response.status);
         }
-        const datos = await response.json();
-        console.log(datos); 
+        const datosNedStark = await response.json();
+        
+        console.log("Información de Ned Stark: ", datosNedStark); 
+
     }catch(error){
-        console.log('Error! ', error);
-    }
-    
+        console.log('Ups, error! ', error);
+        return null;
+    } 
 }
 
 
 //2) Realizar una función que permita recuperar todos los personajes disponibles
 
-async function allCharacters() {
+
+async function todosLosPersonajes(){
 
     try {
-        const response = await fetch (urlApi)
+        const response = await fetch(urlApi);
+
         if (!response.ok){ 
-            throw new Error('Error! ' + response.status);
+            throw new Error('Ups, error! ' + response.status);
         }
-        const datos = await response.json();
+
+        const datosPersonajes = await response.json();
+        return datosPersonajes;
+
+    } catch(error){
+        console.log('Ups, error! ', error);
+        return null;
+    }
+}    
         
-        
+
 //3) Persistir el resultado de la segunda consulta localmente en un archivo JSON
 
-       fs.writeFile('./Characters.json', JSON.stringify(datos, null, 2), (er) => {
-        if (er) {
-            console.error('No se ha escrito el archivo...', er);
-        } else {
-            console.log('El archivo ha sido creado con éxito...');
-        }
-    });
 
-    }catch(error){
-        console.log('Error! ', error);
-    }   
+async function guardarPersonajes(){
+
+    const personajes = await todosLosPersonajes();
+    await fs.writeFile("personajes.json", JSON.stringify(personajes, null, 2));
+    console.log("Yay, guardado! ");
 }
 
 
 //4) Leer el archivo local de personajes
 //a. Mostrar por consola los personajes de la familia Stark. Es decir: “family” = “HouseStark”
 
-function houseStark(){
 
-    fs.readFile('./Characters.json', 'utf-8', (er, data) =>{
-        if (er) {
-            console.error('No se pudo leer el archivo...', er);
-            return;
-    }
-        try{
-            const personajes = JSON.parse(data);
-            const starkCharacters = personajes.filter(character => character.family === 'House Stark');
-            console.log(starkCharacters);
+ async function obtenerFamiliaStark(){
 
+    const datos = await fs.readFile('personajes.json', 'utf-8');
 
-        }catch(parseError) {
-            console.error('Error de parseo...', parseError)}
-    });
-}
+    const personajes = JSON.parse(datos);
+    const familiaStark = personajes.filter((personaje) => personaje.family === "House Stark");
 
+    console.log("Personajes House Stark: ", familiaStark);  
+ } 
 
 //b. Agregar un nuevo personaje y sobrescribir el archivo original
 
-function addCharacter(){
 
-   try{
-    const datos = fs.readFileSync('./Characters.json', 'utf-8');
-    const characters = JSON.parse(datos);
+async function nuevoPersonaje(){
 
-    const newCharacter = {
+    const nuevoPersonaje= {
+
         "id": 53,
-        "firstName": "Daryl",
-        "lastName": "Dixon",
-        "fullName": "Daryl",
-        "title": "Dix",
-        "family": "Dixon",
-        "image": "dd.jpg",
-        "imageUrl": "http://dixon/images/dd.jpg"
-    }
-          
-        characters.push(newCharacter);
-        console.log(characters);
-       
-    }catch(error){
-        console.error('Error al agregar el personaje:', error);
-    }
+        "firstName": "Juan",
+        "lastName": "Perez",
+        "fullName": "Juan Perez",
+        "title": "Juancito",
+        "family": "Perez",
+        "image": "jp.jpg",
+        "imageUrl": "https://juancito/jp.jpg"
+      }
+
+    const datos = await fs.readFile("personajes.json", "utf-8");
+
+    let personajes = JSON.parse(datos);
+    personajes.push(nuevoPersonaje);
+    await fs.writeFile("personajes.json", JSON.stringify(personajes, null, 2));
+
+    console.log("Nuevo personaje agregado! ");
 }
 
 
+//c) Eliminar los personajes cuyo ID sean mayores a 25 y sobrescribir el archivo original
 
 
+async function borrarPersonajeMayor25(){
 
-obtenerNedStark(6);
-allCharacters();
-houseStark();
-addCharacter()
+    const datos = await fs.readFile("personajes.json", "utf8");
+
+    let personajes = JSON.parse(datos);
+    personajes = personajes.filter((personaje) => personaje.id <= 25);
+    await fs.writeFile("personajes.json", JSON.stringify(personajes, null, 2));
+
+    console.log("Elimina los personajes cuyo ID sean mayores a 25");
+  }
+  
+
+
+ (async () => {
+
+    await obtenerNedStark(6);
+    await guardarPersonajes();
+    await obtenerFamiliaStark();
+    await nuevoPersonaje();
+    await borrarPersonajeMayor25();
+    
+  })();
